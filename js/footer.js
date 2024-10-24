15671a7fe277c04d61824815f317e8d74d57da1b
Crisdruetta <133292704+Crisdruetta@users.noreply.github.com>
2024-10-24 15:23:29 -0300

    agregue el footer

--- js/footer.js
+++ js/footer.js
@@ -1,35 +1,5 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const footerContainer = document.getElementById("footer-container");
-
-    fetch("footer.html")
-        .then(response => response.text())
-        .then(data => {
-            footerContainer.innerHTML = data;
-        })
-        .catch(error => {
-            console.error('Error fetching footer:', error);
-        });
-
-    bulmaCarousel.attach('#carousel-demo', {
-        slidesToScroll: 1,
-        slidesToShow: 4,
-        pagination: false,
-        breakpoints: [
-            {
-                changePoint: 480,
-                slidesToShow: 1,
-                slidesToScroll: 1
-            },
-            {
-                changePoint: 640,
-                slidesToShow: 2,
-                slidesToScroll: 2
-            },
-            {
-                changePoint: 768,
-                slidesToShow: 3,
-                slidesToScroll: 3
-            }
-        ]
+fetch('footer.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('footer').innerHTML = data;
     });
-});
